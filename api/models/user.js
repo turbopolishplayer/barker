1219b6f4d9c9b0c45bd07fb199f6f8dcb49d2515
turbopolishplayer <turbopolishplayer@gmail.com>
2020-07-16 17:59:21 +0200

    Add UserModel with createUserFunction

---
+++ api/models/user.js
@@ -0,0 +1,45 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+
+
+mongoUrl = 'mongodb://localhost:27017/barker'
+
+
+async function createUser(email, name, lastname, password){
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = {
+        email: email,
+        name: name,
+        lastname: lastname,
+        password: hashPassword
+    }
+
+    let client;
+    try{
+        client = await dbConnect();
+    }catch(error){
+        throw error;
+    }
+
+    const db = client.db();
+    try{
+        await db.collection('users').insertOne(user);
+    }catch(error){
+        console.log(error.message)
+    }
+    client.close();
+}
+
+
+async function dbConnect(){
+    let dbconnection;
+    try{
+        dbconnection = await MongoClient.connect(mongoUrl, { useUnifiedTopology: true });
+    }catch(error){
+        throw error;
+    }
+
+    return dbconnection;
+}
+
+createUser('asd@asd.pl', 'dawid', 'wengrzik', 'zupa');
